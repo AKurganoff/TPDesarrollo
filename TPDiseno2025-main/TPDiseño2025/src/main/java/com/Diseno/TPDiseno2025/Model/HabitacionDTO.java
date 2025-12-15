dc098e4f950036bfb861fb30908669d18d302d18
maximom29 <150258297+maximom29@users.noreply.github.com>
2025-12-15 09:30:22 -0300

    Update HabitacionDTO.java

--- TPDiseno2025-main/TPDiseño2025/src/main/java/com/Diseno/TPDiseno2025/Model/HabitacionDTO.java
+++ TPDiseno2025-main/TPDiseño2025/src/main/java/com/Diseno/TPDiseno2025/Model/HabitacionDTO.java
@@ -3,6 +3,7 @@ package com.Diseno.TPDiseno2025.Model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,6 +21,7 @@ public class HabitacionDTO {
     private Integer idTipo;
 
     @NotNull
+    @PositiveOrZero
     private Integer nochesDescuento;
 
     @NotBlank
