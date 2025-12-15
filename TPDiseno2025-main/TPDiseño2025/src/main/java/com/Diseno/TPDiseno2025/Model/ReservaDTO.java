69900a56fa4066a9d8c405f2470d7ba106f73244
maximom29 <150258297+maximom29@users.noreply.github.com>
2025-12-15 09:29:55 -0300

    Update ReservaDTO.java

--- TPDiseno2025-main/TPDiseño2025/src/main/java/com/Diseno/TPDiseno2025/Model/ReservaDTO.java
+++ TPDiseno2025-main/TPDiseño2025/src/main/java/com/Diseno/TPDiseno2025/Model/ReservaDTO.java
@@ -1,9 +1,8 @@
 package com.Diseno.TPDiseno2025.Model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,32 +11,31 @@ import lombok.Setter;
 @Setter
 public class ReservaDTO {
 
-    // En CREATE puede venir null (la BD lo genera). Si viene, que sea > 0.
+   
     @Positive
     private Integer idReserva;
 
-    @NotBlank
+    @NotNull
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "fechaInicio debe tener formato yyyy-MM-dd")
     @Size(max = 10)
-    // formato YYYY-MM-DD
-    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "fechaInicio debe tener formato YYYY-MM-DD")
     private String fechaInicio;
 
     @NotNull
-    @Positive
+    @Positive(message = "cantNoches debe ser > 0")
     private Integer cantNoches;
 
-    @NotNull
+   
     @Positive
     private Integer idHuesped;
 
     @NotNull
-    @Positive
+    @Positive(message = "cantHuesped debe ser > 0")
     private Integer cantHuesped;
 
     @NotNull
     private Boolean descuento;
 
-    @NotBlank
+   
     @Size(max = 10)
     private String estado;
 }
